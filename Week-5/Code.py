e65e4d79b3fd4640616176f958055d9fb70486dc
DerronMendel2001 <derron@themendels.org>
2021-03-07 18:18:39 +0200

    First Day of Python, list of .bash commands

---
+++ Week-5/Code.py
@@ -0,0 +1,43 @@
+# Exercises XP
+
+# a = 10
+# b = 12
+# if a > b:
+# 	print("Hello World")
+
+# number = int(input("Please enter a number."))
+
+# if number % 2 == 0:
+# 	print(f"{number} is even.")
+# elif number % 2 == 1:
+# 	print(f"{number} is odd.")
+
+# userName = input("Enter your name. ")
+# if userName == "Derron" or userName == "derron":
+# 	print("Nice to meet you, Daren!")
+# elif userName == "Daren" or userName == "daren" or userName == "Darren" or userName == "darren":
+# 	print("GET OUT. NOW.")
+# elif userName == "Bob" or userName == "bob":
+# 	print("Nice to meet you, Joe!")
+# else:
+# 	print("Nice to meet you, Bob!")
+
+# height = int(input("Enter your height in inches. ")) * 2.54
+
+# if height > 145:
+# 	print("Have a fun trip!")
+# else:
+# 	print("Sorry, you're not tall enough to ride this.")
+
+# Daily Challenge!!!
+
+# string = input("Enter a string of exactly 10 characters. ")
+# if len(string) > 10:
+# 	print("String too long.")
+# elif len(string) < 10:
+# 	print("String too short.")
+# else:
+# 	for i in range(0, len(string)+1):
+# 		print(string[:i])
+
+print("Hello World")
